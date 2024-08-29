72b11d88269b2bff1f88266311326a99995e6244
Jatinverma9728 <somveerparjapati123456@gmail.com>
2024-08-29 23:11:17 +0530

    gsap animation add on nav bar and main text

--- sitejs/sidemenu.js
+++ sitejs/sidemenu.js
@@ -5,3 +5,23 @@ function openmenu() {
 function closemenu() {
   sidemeu.style.right = "-200px";
 }
+
+var tl = gsap.timeline();
+tl.from("#header img", {
+  y: -30,
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+});
+
+tl.from("#header li", {
+  y: -30,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.3,
+});
+tl.from(".header-text span", {
+  x: -300,
+  opacity: 0,
+  duration: 0.2,
+});
